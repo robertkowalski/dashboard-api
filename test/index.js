01a60eab5951d6eae2d463c06d61caea5733b53b
Robert Kowalski <rok@kowalski.gd>
2014-08-07 19:39:19 +0200

    init

---
+++ test/index.js
@@ -0,0 +1,13 @@
+var Lab = require("lab"),
+    lab = exports.lab = Lab.script(),
+    describe = lab.experiment,
+    before = lab.before,
+    it = lab.test,
+    assert = require("assert")
+
+describe("API", function () {
+  it("should start a server which returns a 200 on /", function (done) {
+    assert.ok(false, "not implemented")
+    done()
+  })
+})
