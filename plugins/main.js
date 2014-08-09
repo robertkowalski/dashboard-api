9277125c84bdd15ad7708c07870c273f295f7101
Robert Kowalski <rok@kowalski.gd>
2014-08-09 16:34:52 +0200

    add btc/litecoin stats

--- plugins/main.js
+++ plugins/main.js
@@ -1,10 +1,21 @@
-exports.register = function Main (plugin, options, next) {
+var p = require("peanuts").blockr,
+    env = process.env
+
+exports.register = function Main (plugin, opts, next) {
+  const BTC_ADDRESSES = opts.btcWallets || env.BTC_ADRESSES.split(",")
+  const LTC_ADRRESSES = opts.ltcWallets || env.LTC_ADRESSES.split(",")
+  const blockOpts = opts.blockchainOptions || {}
 
   plugin.route({
     path: "/",
     method: "GET",
     handler: function (request, reply) {
-      reply({"ok": true})
+      p("btc", BTC_ADDRESSES, blockOpts, function (err, btc) {
+        p("ltc", LTC_ADRRESSES, blockOpts, function (er, ltc) {
+
+          reply({"ltc": ltc, "btc": btc})
+        })
+      })
     }
   })
 
--- test/index.js
+++ test/index.js
@@ -6,15 +6,24 @@ var Lab = require("lab"),
     it = lab.test,
     assert = require("assert"),
     request = require("request"),
-    Hapi = require("hapi")
+    Hapi = require("hapi"),
+    hock = require("hock")
 
 const PORT = 1337
-
+const MOCK_BLOCKCHAIN_PORT = 1338
 var server
+
 before(function (done) {
   server = Hapi.createServer(PORT)
   server.pack.register([{
-      plugin: require('../plugins/main.js')
+      plugin: require('../plugins/main.js'),
+      options: {
+        blockchainOptions: {
+          url: "http://localhost:" + MOCK_BLOCKCHAIN_PORT + "/"
+        },
+        btcWallets: [1, 2],
+        ltcWallets: [1, 2]
+      }
     }], function (err) {
       if (err) throw err
       server.start(function () {
@@ -27,14 +36,56 @@ after(function (done) {
   server.stop(done)
 })
 
+function createMockServer (url, cb) {
+  hock.createHock(MOCK_BLOCKCHAIN_PORT, function (err, hockServer) {
+    hockServer
+        .get(url)
+        .many()
+        .reply(200, {
+          "status": "success",
+          "data": [
+            {
+              "address": "1",
+              "balance": 42,
+              "balance_multisig": 0
+            },
+            {
+              "address": "2",
+              "balance": 23,
+              "balance_multisig": 0
+            }
+          ],
+          "code": 200,
+          "message": ""
+        })
+    cb(null, hockServer)
+  })
+}
+
 describe("API", function () {
-  it("should returns a 200 on /", function (done) {
-    request({
-      uri: "http://localhost:" + PORT + "/",
-      json: true
-    }, function (err, res, body) {
-      assert.equal(res.statusCode, 200)
-      done()
+  it("returns a 200 on /", function (done) {
+    createMockServer("/1,2", function (er, s) {
+      request({
+        uri: "http://localhost:" + PORT + "/",
+        json: true
+      }, function (err, res, body) {
+        assert.equal(res.statusCode, 200)
+        s.close()
+        done()
+      })
+    })
+  })
+  it("returns wallet data", function (done) {
+    createMockServer("/1,2", function (er, s) {
+      request({
+        uri: "http://localhost:" + PORT + "/",
+        json: true
+      }, function (err, res, body) {
+        assert.ok(body.ltc[0])
+        assert.ok(body.btc[0])
+        s.close()
+        done()
+      })
     })
   })
 })
